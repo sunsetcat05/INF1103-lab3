fdf65c6a9865fd3efe73963325aaf1033a291ea6
Zoey <2605299@sit.singaporetech.edu.sg>
2026-09-18 10:00:16 +0800

    Second Commit

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,23 +1,55 @@
 inventory = 0
 Failed = 0
 
-stock = input("Please enter the stock quantity:")
+stock = get_valid_Input()
 
-while stock != "quit":   
-    if not stock.isdigit():
+def get_valid_Input():
+    while True:
+        stock = input("Please enter the stock quantity:")
+        if stock == "quit":
+            return stock
+        elif stock.isdigit():
+            return int(stock)
+        else:
+            print("Invalid stock quantity. Please enter again.")
+
+def process_quantity(stock):
+    global inventory, Failed
+    if stock == "quit":
+        return
+    elif isinstance(stock, int):
+        inventory += stock
+        print("Current Stock Quantity:", inventory)
+    else:
         print("Invalid stock quantity. Please enter again.")
         Failed += 1
         print("Number of Failed/Rejected Entries:", Failed, "units")
-    else:
-        inventory += int(stock)
-        print("Current Stock Quantity:", inventory)
-    if inventory > 500:
-        print("Inventory is exceeded.")
-        break
-    if inventory == 500:
-        print("Maximum inventory reached. No more stock can be added.")
-        break
-    stock = input("Please enter the stock quantity:")
+
+def calculate_tax(amount):
+    tax_rate = 0.1  # 10% tax rate
+    tax = amount * tax_rate
+    return tax
+
+def generate_report():
+    print("Total Units Processed:", inventory)
+    print("Number of Failed/Rejected Entries:", Failed, "units")
+    tax = calculate_tax(inventory)
+    print("Total Tax on Inventory:", tax)
+# while stock != "quit":   
+#     if not stock.isdigit():
+#         print("Invalid stock quantity. Please enter again.")
+#         Failed += 1
+#         print("Number of Failed/Rejected Entries:", Failed, "units")
+#     else:
+#         inventory += int(stock)
+#         print("Current Stock Quantity:", inventory)
+#     if inventory > 500:
+#         print("Inventory is exceeded.")
+#         break
+#     if inventory == 500:
+#         print("Maximum inventory reached. No more stock can be added.")
+#         break
+#     stock = input("Please enter the stock quantity:")
 
 print("Total Units Processed:", inventory)
 print ("Number of Failed/Rejected Entries:", Failed, "units")
